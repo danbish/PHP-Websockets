c53d2de249d0b108c774be46477d0c03e7eed860
Dan Bish <dbish_career@yahoo.com>
2014-10-07 13:13:48 -0400

    just tinkering - ignore this version

--- testwebsock.php
+++ testwebsock.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-require_once('./websockets.php');
+require_once( './websockets.php' );
 
 class echoServer extends WebSocketServer 
 {
@@ -11,10 +11,28 @@ class echoServer extends WebSocketServer
    * Called immediately when the data is recieved.
    * Defined as an acstract in websocket server
    */
-  protected function process ($user, $message) 
+  protected function process ( $user, $message ) 
   {
     $this->send($user,$message);
   }
+  
+/* just working on this - src: http://www.abrandao.com/2013/06/25/websockets-using-modern-html5-technology-for-true-server-push/
+function process($user,$msg){
+  $action = unwrap($msg);
+  say("< ".$action);
+  switch($action){
+    case "hello" : send($user->socket,"hello human"); break;
+    case "hi" : send($user->socket,"zup human"); break;
+    case "name" : send($user->socket,"my name is Multivac, silly I know"); break;
+    case "age" : send($user->socket,"I am older than time itself"); break;
+    case "date" : send($user->socket,"today is ".date("Y.m.d")); break;
+    case "time" : send($user->socket,"server time is ".date("H:i:s")); break;
+    case "thanks": send($user->socket,"you're welcome"); break;
+    case "bye" : send($user->socket,"bye"); break;
+    default : send($user->socket,$action." not understood"); break;
+  }
+}*/
+  
   
   /**
    * Called after the handshake response is sent to the client.
